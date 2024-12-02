e1737f403b48fdf2a81c8c9f289b1fcd5602ee07
Denis Prilukov <prilukov2002@mail.ru>
2024-12-02 16:26:00 +0500

    Конец работы над строками

---
+++ Практика 3/02-string.js
@@ -0,0 +1,38 @@
+// const firstName = "Denis";
+// const age = 22;
+
+// const old = "hello my name is " + name;
+// const output = `any
+// sdfsd
+// string`;
+
+// let newStr = `Hello my name is ${name} and my age is ${
+//   age > 18 ? "Can drive" : "Can't drive"
+// }`;
+
+// console.log(output); // все пробелы сохранятся даже энтеры
+// console.log(old); // старые способы соединения в строках - конкатенация
+// console.log(newStr); // строка выставлена. можно использовать и функции, главное не забудь про ее вызов через ()
+
+// console.log(firstName.length); // 5
+// console.log(firstName.toUpperCase()); // DENIS, саму строку не мутирует
+// console.log(firstName.toLowerCase()); // denis, также
+
+// console.log(firstName.charAt(2)); // n. передает символ, исходя из его индекса
+// console.log(firstName.charAt(99)); // ''
+
+// console.log(firstName.indexOf("n")); // 2
+// console.log(firstName.indexOf("k")); // -1
+// console.log(firstName.indexOf("Denis")); // 0
+
+// console.log(firstName.startsWith("den")); // false
+// console.log(firstName.startsWith("Den")); // true
+// console.log(firstName.toLowerCase().startsWith("den")); // true
+
+// console.log(firstName.endsWith("is")); // true
+// console.log(firstName.repeat(2)); // DenisDenis
+
+// const password = "      my pass         ";
+
+// console.log(password.trim()); // my pass - удаляет все лишние пробелы
+// console.log(password.trim().padStart(5)); //      my pass - добавляет в начало 5 пробелов
